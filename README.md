7845559d84d31f9d52bf74469fc0a6f9c01389ca
fffzlfk <1319933925qq@gmail.com>
2022-08-09 17:25:44 +0800

    feat: add web

--- README.md
+++ README.md
@@ -13,4 +13,23 @@ pip install pic_resizer
 
 ## Usage
 
-[![asciicast](https://asciinema.org/a/3j8qHTjMYUPif9nLMWaOwYzU4.svg)](https://asciinema.org/a/3j8qHTjMYUPif9nLMWaOwYzU4)
+```console
+~ ➜ pic_resizer --help
+Usage: pic_resizer [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --version  Show the version and exit.
+  --help     Show this message and exit.
+
+Commands:
+  cli  Operating from the command line
+  web  Operating in the web app
+```
+
+### cli
+
+[![asciicast](https://asciinema.org/a/d5G7DaIrDN048rrLGr1TbJDh1.svg)](https://asciinema.org/a/d5G7DaIrDN048rrLGr1TbJDh1)
+
+### web
+
+![](https://s3.bmp.ovh/imgs/2022/08/09/1d0c289413d7078e.png)
--- pic_resizer/entry.py
+++ pic_resizer/entry.py
@@ -1,61 +1,56 @@
-import io
-from logging import debug, error
+from logging import debug
 import logging
 import os
 import sys
 import click
 from PIL import Image
-from .utils import filepath2basename_without_extension, parse_file_size
+from .utils import parse_file_size
+from .lib import image_resizing
+from .web import serve
+from .utils import filepath2basename_without_extension
+from . import __version__
 
 
-def image_resizing(origin: Image, target_size: int, file: str):
-    (width, height) = (origin.size[0], origin.size[1])
-    aspect_ratio = width / height
-    l, r = 1, height
-    for _ in range(10000):
-        mid = (l + r) // 2
-        resized = origin.resize([int(aspect_ratio * mid), mid])
-        out = io.BytesIO()
-        resized.save(out, format=origin.format, quality=95, subsampling=0)
-        size = out.tell()
-        debug(f"mid={mid}, size={size}")
-        if size <= target_size:
-            if target_size - size < target_size * 0.01:
-                basename = filepath2basename_without_extension(file)
-                resized.save(
-                    f"{basename}_resized.{origin.format.lower()}",
-                    format=origin.format,
-                    quality=95,
-                    subsampling=0,
-                )
-                break
-            l = mid + 1
-        else:
-            r = mid
-        out.flush()
-    else:
-        error("timeout")
-        sys.exit(1)
-
-
-@click.command()
+@click.command(help="Operating from the command line")
 @click.argument("file")
 @click.option(
     "--size",
     "-s",
-    required=True,
     type=str,
     default="100k",
-    help="size of target image file, eg: 100k, 1M...",
+    help="Size of target image file, eg: 100k, 1M...",
 )
-def run(file: str, size):
+def cli(file: str, size):
     assert os.path.isfile(file), f"{file} is not a file"
     target_size = parse_file_size(size)
-    assert os.stat(file).st_size > target_size, "image file is too small"
+    assert os.stat(file).st_size > target_size, "Image file is too small"
     with Image.open(file) as im:
-        assert target_size, "target size is wrong"
+        assert target_size, "Target size is wrong"
+        assert target_size >= 2000, "Target size is too small"
         debug(f"target_size={target_size}")
-        image_resizing(im, target_size, file)
+        basename = filepath2basename_without_extension(file)
+        image_resizing(im, target_size).save(
+            f"{basename}_resized.{im.format.lower()}",
+            format=im.format,
+            quality=75,
+            subsampling=0,
+        )
+
+
+@click.command(help="Operating in the web app")
+def web():
+    click.echo("in web")
+    serve()
+
+
+@click.version_option(__version__)
+@click.group()
+def run():
+    pass
+
+
+run.add_command(cli)
+run.add_command(web)
 
 
 def main():
